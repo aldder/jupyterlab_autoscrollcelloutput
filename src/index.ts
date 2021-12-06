2d41241b693a87ca791e076bbbdbe17a887e7fd3
Aldo <aldo.deramo@gmail.com>
2021-12-06 12:45:42 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,72 @@
+import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application';
+import { IDisposable, DisposableDelegate } from '@lumino/disposable';
+import { ToolbarButton } from '@jupyterlab/apputils';
+import { DocumentRegistry } from '@jupyterlab/docregistry';
+import { NotebookPanel, INotebookModel } from '@jupyterlab/notebook';
+
+
+function JupyterLabAutoScroll() {
+  let divs = document.querySelectorAll('.lm-Widget.p-Widget.jp-OutputArea.jp-Cell-outputArea');
+  for(let i=0; i<divs.length; i++) {
+    let div = divs[i];
+    if(div.scrollHeight > 0 && 
+       div.scrollTop != div.scrollHeight &&
+       div.parentElement != null &&
+       div.parentElement.parentElement != null &&
+       div.parentElement.parentElement.childNodes[1].childNodes[1].childNodes[0].childNodes[0].textContent == "[*]:") {
+      if(div.childNodes.length == 1) {
+        div.scrollTop = div.scrollHeight;
+      }
+    }
+  }
+}
+
+/**
+ * Initialization data for the jupyterlab_autoscrollcelloutput extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab_autoscrollcelloutput:plugin',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    console.log('JupyterLab extension jupyterlab_autoscrollcelloutput is activated!');
+    app.docRegistry.addWidgetExtension('Notebook', new ButtonAutoScrollCellOutput());
+  }
+};
+
+export class ButtonAutoScrollCellOutput
+  implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel>
+  {
+    createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
+      
+      
+      const triggerAutoScrollCellOutput = () => {
+        if (SET) {
+          SET = false;
+          if (button.hasClass('selected')) button.removeClass('selected');
+          clearInterval(t);
+        }
+        else {
+          SET = true;
+          button.addClass('selected');
+          t = setInterval(JupyterLabAutoScroll, 10);
+        }
+      };
+
+      const button = new ToolbarButton({
+        className: 'buttonAutoScrollCellOuput',
+        iconClass: 'wll-ScrollIcon',
+        label: 'scroll',
+        onClick: triggerAutoScrollCellOutput,
+        tooltip: 'Auto Scroll Cell Ouput'
+      })
+
+      panel.toolbar.insertItem(10, 'AutoScrollCellOutput', button);
+
+      var SET = true;
+      var t = setInterval(JupyterLabAutoScroll,10);
+      button.addClass('selected');
+      return new DisposableDelegate(() => { button.dispose(); });
+    }
+  }
+
+export default plugin;

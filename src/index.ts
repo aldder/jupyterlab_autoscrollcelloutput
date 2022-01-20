879a29b67fbb670ab203765f17e20444957daff8
Aldo <aldo.deramo@gmail.com>
2022-01-20 20:24:39 +0100

    rebuild engine avoiding javascript code

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,84 @@
 import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application';
 import { IDisposable, DisposableDelegate } from '@lumino/disposable';
 import { ToolbarButton } from '@jupyterlab/apputils';
+import { ICellModel, CodeCellModel, CodeCell } from '@jupyterlab/cells';
+import { IObservableList } from '@jupyterlab/observables';
 import { DocumentRegistry } from '@jupyterlab/docregistry';
-import { NotebookPanel, INotebookModel } from '@jupyterlab/notebook';
+import { NotebookPanel, INotebookModel, Notebook } from '@jupyterlab/notebook';
+import { each } from '@lumino/algorithm';
 
 
-function JupyterLabAutoScroll() {
-  let divs = document.querySelectorAll('.lm-Widget.p-Widget.jp-OutputArea.jp-Cell-outputArea');
-  for(let i=0; i<divs.length; i++) {
-    let div = divs[i];
-    if(div.scrollHeight > 0 && 
-       div.scrollTop != div.scrollHeight &&
-       div.parentElement != null &&
-       div.parentElement.parentElement != null &&
-       div.parentElement.parentElement.childNodes[1].childNodes[1].childNodes[0].childNodes[0].textContent == "[*]:") {
-      if(div.childNodes.length == 1) {
-        div.scrollTop = div.scrollHeight;
-      }
-    }
-  }
-}
-
-/**
- * Initialization data for the jupyterlab_autoscrollcelloutput extension.
- */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab_autoscrollcelloutput:plugin',
-  autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension jupyterlab_autoscrollcelloutput is activated!');
-    app.docRegistry.addWidgetExtension('Notebook', new ButtonAutoScrollCellOutput());
-  }
+    id: 'jupyterlab_autoscrollcelloutput:plugin',
+    autoStart: true,
+    activate: (app: JupyterFrontEnd) => {
+        console.log('JupyterLab extension jupyterlab_autoscrollcelloutput is activated!');
+        app.docRegistry.addWidgetExtension('Notebook', {
+            createNew: (panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable => {
+                return new ButtonAutoScrollCellOutput().init(panel);
+            }
+        });
+    }
 };
 
-export class ButtonAutoScrollCellOutput
-  implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel>
-  {
-    createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
-      
-      
-      const triggerAutoScrollCellOutput = () => {
-        if (SET) {
-          SET = false;
-          console.log('Extension jupyterlab_autoscrollcelloutput disabled');
-          clearInterval(t);
-          if (button.hasClass('selected')) button.removeClass('selected');
-        }
-        else {
-          SET = true;
-          console.log('Extension jupyterlab_autoscrollcelloutput enabled');
-          t = setInterval(JupyterLabAutoScroll, 10);
-          button.addClass('selected');
-        }
-      };
 
-      const button = new ToolbarButton({
-        className: 'buttonAutoScrollCellOuput',
-        iconClass: 'wll-ScrollIcon',
-        label: 'scroll',
-        onClick: triggerAutoScrollCellOutput,
-        tooltip: 'Auto Scroll Cell Ouput'
-      })
+class ButtonAutoScrollCellOutput {
+    private notebook!: Notebook;
 
-      panel.toolbar.insertItem(10, 'AutoScrollCellOutput', button);
+    init(panel: NotebookPanel): IDisposable {
 
-      var SET = true;
-      var t = setInterval(JupyterLabAutoScroll,10);
-      button.addClass('selected');
-      return new DisposableDelegate(() => { button.dispose(); });
+        const triggerAutoScrollCellOutput = () => {
+            if (SET) {
+                SET = false;
+                console.log('Extension jupyterlab_autoscrollcelloutput disabled for notebook:', panel.id);
+                if (button.hasClass('selected')) button.removeClass('selected');
+            }
+            else {
+                SET = true;
+                console.log('Extension jupyterlab_autoscrollcelloutput enabled for notebook:', panel.id);
+                button.addClass('selected');
+            }
+            this.notebook.model!.metadata.set('autoscrollcelloutput', SET);
+        };
+
+        const button = new ToolbarButton({
+            className: 'buttonAutoScrollCellOuput',
+            iconClass: 'wll-ScrollIcon',
+            label: 'scroll',
+            onClick: triggerAutoScrollCellOutput,
+            tooltip: 'Auto Scroll Cell Ouput'
+        })
+
+        panel.toolbar.insertItem(10, 'AutoScrollCellOutput', button);
+        this.notebook = panel.content;
+        var SET = true;
+        button.addClass('selected');
+        this.notebook.model!.cells.changed.connect(this.handlerCellsChange, this);
+        return new DisposableDelegate(() => { button.dispose(); });
     }
-  }
+ 
+    private handlerCellsChange(
+        cells: IObservableList<ICellModel>,
+        changed_cells: IObservableList.IChangedArgs<ICellModel>): void {
+        if (changed_cells.type == 'add') {
+            each(changed_cells.newValues, (cellModel, idx) => {
+                if (cellModel instanceof CodeCellModel) {
+                    cellModel.outputs.changed.connect((output, arg) => {
+                        let autoScrollSet = this.notebook.model!.metadata.get('autoscrollcelloutput');
+                        if (['add', 'set'].includes(arg.type) && cellModel.metadata.get("scrolled") && autoScrollSet) {
+                            // Find the widget for the model.
+                            for (let cell of this.notebook.widgets) {
+                                if (cell instanceof CodeCell && cell.model == cellModel) {
+                                    // Scroll to bottom
+                                    cell.outputArea.node.scrollTop = cell.outputArea.node.scrollHeight;
+                                }
+                            }
+                        }
+                    });
+                }
+            });
+        }
+    }
+}
 
 export default plugin;
